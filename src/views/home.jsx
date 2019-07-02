dbbb7a1a0f48fc2614458016032ab4e333200158
forGit <forheyin@163.com>
2019-07-02 23:19:50 +0800

    css

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import '../styles/main.scss'
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            arr: []
-        };
+  constructor(props) {
+    super(props)
+    this.state = {
+      arr: []
     }
+    this.cstat = 444
+  }
 
-    render() {
-        return (
-            <div className="homePage">
-                haha
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="mian">
+        <div>我是单色</div>
+      </div>
+    )
+  }
 }
 
-export default Home;
+export default Home
