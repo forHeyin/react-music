59dedcebe1134af7b8e59e79186096983c3c1510
HitMeIdiot <async_await@163.com>
2019-07-23 23:24:06 +0800

    [feat]头部

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -11,9 +11,33 @@ class Head extends Component {
     return (
       <div className="head">
         <div className="wapper wapper1">网易云音乐</div>
-        <div className="wapper wapper2"> </div>
-        <div className="wapper wapper3"> </div>
-        <div className="wapper wapper4"> </div>
+        <div className="wapper wapper2">
+          <div className="w1">
+            <span className="iconfont icon-arrowleft" />
+            <span className="iconfont icon-arrowright" />
+          </div>
+          <div className="w2">
+            <input type="text" placeholder="搜索音乐，视频，歌词，电台" />
+            <span className="iconfont icon-search" />
+          </div>
+          <div className="w3">
+            <img src="" alt="" />
+            <span>测试</span>
+            <span className="iconfont icon-xiala" />
+          </div>
+        </div>
+        <div className="wapper wapper3">
+          <div>
+            <span className="iconfont icon-xiala" />
+            <span className="iconfont icon-xiala" />
+            <span className="iconfont icon-xiala" />
+          </div>
+          <div>
+            <span className="iconfont icon-xiala" />
+            <span className="iconfont icon-xiala" />
+            <span className="iconfont icon-xiala" />
+          </div>
+        </div>
       </div>
     )
   }
